7facb012e33e80867cbbc3895dc41b57b415a063
Danilo Godinho <danilo.godinho@onebox.one>
2023-03-28 09:13:54 -0300

    Barra lateral de navegacao e bootstrap instalado

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="col-3 bg-success">
+    <BarraLateral />
+  </header>
+</template>
+
+<script setup lang="ts">
+import BarraLateral from './components/BarraLateral.vue';
+
+</script>
+
+<style>
+header {
+  height: 100vh;
+}
+</style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,33 @@
+<template>
+    <h1 class="text-center p-1">Carango Bom</h1>
+    <nav class="text-center">
+        <ul class="navbar-nav">
+            <li class="nav-item p-1">
+                <a class="nav-link bg-primary text-light" href="#">Entrar</a>
+            </li>
+            <li class="nav-item p-1">
+                <a class="nav-link bg-primary text-light" href="#">Veículos</a>
+            </li>
+            <li class="nav-item p-1">
+                <a class="nav-link bg-primary text-light" href="#">Marcas</a>
+            </li>
+            <li class="nav-item p-1">
+                <a class="nav-link bg-primary text-light" href="#">Usuários</a>
+            </li>
+            <li class="nav-item p-1">
+                <a class="nav-link bg-primary text-light" href="#">Dashboard</a>
+            </li>
+            <li class="nav-item p-1">
+                <a class="nav-link bg-primary text-light" href="#">Sair</a>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style>
+
+</style>
