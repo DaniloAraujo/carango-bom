ca88a2421c01eb877f8228cc14472c11fc4454a3
Bruno <brunogoncalves@outlook.com.br>
2023-03-30 23:17:15 -0300

    inicia implementação do sistema de login

---
+++ client/src/services/autenticacao-service.ts
@@ -0,0 +1,19 @@
+export function autentica(email: string, senha: string, store: any) {
+  const credenciais = { email, senha }
+
+  return fetch(import.meta.env.VITE_BASE + "/api/login", {
+    method: "POST", 
+    headers: {
+      "content-type": "application/json"
+    },
+    body: JSON.stringify(credenciais)
+  })
+  .then(resp => resp.json())
+  .then(resp => {
+    sessionStorage.setItem("token", resp.dados.token)
+    store.usuario = resp.dados.usuario
+
+    return resp.dados.usuario
+  })
+  .catch(e => Promise.reject(e.message))
+}
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,35 +1,68 @@
 <template>
-    <section class="vh-100">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center h-100">
-                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                    <div class="card shadow-2-strong">
-                        <div class="card-body p-5 text-center bg-dark rounded">
-                            <h3 class="mb-5 text-light">Sign in</h3>
+  <section class="vh-100">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center h-100">
+        <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+          <div class="card shadow-2-strong">
+            <form @submit.prevent="autenticaUsuario" class="card-body p-5 text-center bg-dark rounded">
+              <h3 class="mb-5 text-light">Sign in</h3>
 
-                            <div class="form-outline mb-4">
-                                <input type="email" id="typeEmailX-2" class="form-control form-control-lg" />
-                                <label class="form-label text-light" for="typeEmailX-2">Email</label>
-                            </div>
+              <div class="form-outline mb-4">
+                <input
+                  type="email"
+                  id="typeEmailX-2"
+                  class="form-control form-control-lg"
+                  v-model="email"
+                />
+                <label class="form-label text-light" for="typeEmailX-2">Email</label
+                >
+              </div>
 
-                            <div class="form-outline mb-4">
-                                <input type="password" id="typePasswordX-2" class="form-control form-control-lg" />
-                                <label class="form-label text-light" for="typePasswordX-2">Senha</label>
-                            </div>
+              <div class="form-outline mb-4">
+                <input
+                  type="password"
+                  id="typePasswordX-2"
+                  class="form-control form-control-lg"
+                  v-model="senha"
+                />
+                <label class="form-label text-light" for="typePasswordX-2"
+                  >Senha</label
+                >
+              </div>
 
-                            <button class="btn btn-primary btn-lg" type="submit">Entrar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+              <button class="btn btn-primary btn-lg" type="submit">
+                Entrar
+              </button>
+            </form>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { autentica } from '../services/autenticacao-service.js'
+import { useUsuarioStore } from '../stores/useUsuarioStore.js'
+import { useRouter } from 'vue-router'
+
+const email = ref("")
+const senha = ref("")
+const usuarioStore = useUsuarioStore()
+
+const router = useRouter()
+
+function autenticaUsuario() {
+    autentica(email.value, senha.value, usuarioStore)
+      .then(() => router.push("/dashboard"))
+      .catch(e => {
+        alert("Algo deu errado na autenticação ;(")
+        console.log('fetchErr::', e)
+      })
+}
 
 </script>
 
 <style>
-
 </style>
